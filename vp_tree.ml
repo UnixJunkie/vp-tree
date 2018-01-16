a19f9129f479b5f05c578c2ff61e186a7b7a1a0b
Francois BERENGER <ligand@free.fr>
2018-01-16 09:41:35 +0900

    Vp_tree: moved out Array extending code into MyArray

--- vp_tree.ml
+++ vp_tree.ml
@@ -1,4 +1,5 @@
 
+module A = MyArray
 module L = List
 
 (* Vantage-point tree implementation
@@ -8,79 +9,6 @@ module L = List
    download?doi=10.1.1.41.4193&rep=rep1&type=pdf *)
 
 (* Functorial interface *)
-
-(* extend the Array module *)
-module A =
-struct
-  include Array
-
-  (* smaller array, without elt at index 'i' *)
-  let remove i a =
-    let n = length a in
-    assert(i >= 0 && i < n);
-    let res = make (n - 1) a.(0) in
-    let j = ref 0 in
-    for i' = 0 to n - 1 do
-      if i' <> i then
-        (unsafe_set res !j (unsafe_get a i');
-         incr j)
-    done;
-    res
-
-  (* <=> List.partition *)
-  let partition p a =
-    let ok, ko =
-      fold_right (fun x (ok_acc, ko_acc) ->
-          if p x then (x :: ok_acc, ko_acc)
-          else (ok_acc, x :: ko_acc)
-        ) a ([], [])
-    in
-    (of_list ok, of_list ko)
-
-  (* <=> List.split *)
-  let split a =
-    let n = length a in
-    if n = 0 then ([||], [||])
-    else
-      let l, r = a.(0) in
-      let left = make n l in
-      let right = make n r in
-      for i = 1 to n - 1 do
-        let l, r = a.(i) in
-        unsafe_set left i l;
-        unsafe_set right i r
-      done;
-      (left, right)
-
-  (* <=> BatArray.min_max with default value in case of empty array *)
-  let min_max_def a def =
-    let n = length a in
-    if n = 0 then def
-    else
-      let mini = ref a.(0) in
-      let maxi = ref a.(0) in
-      for i = 0 to n - 1 do
-        let x = a.(i) in
-        if x < !mini then
-          mini := x;
-        if x > !maxi then
-          maxi := x
-      done;
-      (!mini, !maxi)
-
-  (* get one bootstrap sample of 'size' using sampling with replacement *)
-  let bootstrap_sample rng size a =
-    let n = length a in
-    assert(n > 0);
-    assert(size < n);
-    let res = make size a.(0) in
-    for i = 0 to size - 1 do
-      let rand = Random.State.int rng n in
-      unsafe_set res i (unsafe_get a rand)
-    done;
-    res
-
-end
 
 module type Point =
 sig
